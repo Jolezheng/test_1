142bc7fb425a240f3729d83623938bedcbab8b7d
Jolezheng <74344634+Jolezheng@users.noreply.github.com>
2021-02-25 11:58:04 +0800

    Add files via upload

---
+++ file_report.py
@@ -0,0 +1,14 @@
+import os
+
+class Write(object):
+	file_name='report.txt'
+
+	test_report = open(file_name,'a',encoding='utf-8') 
+		# file_obj.write('')
+		# file_obj.write("")
+		# file_obj.write("")
+
+
+		
+
+	
